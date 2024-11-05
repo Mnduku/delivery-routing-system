bb1408c99475d12f461428ad19e295daac8ae6b0
Mnduku <116856099+Mnduku@users.noreply.github.com>
2024-11-05 14:08:56 -0500

    Added variable

--- truck.py
+++ truck.py
@@ -3,7 +3,7 @@ import datetime
 # Represents good use of encapsulation
 class Truck:
     def __init__(self, id, max_load, velocity, cargo, shipments, distance_travelled, departure_time, current_time,
-                 current_address):
+                 current_address, required_packages):
         self.id = id  # Add the id attribute
         self.max_load = max_load
         self.velocity = velocity
@@ -14,9 +14,12 @@ class Truck:
         self.departure_time = departure_time
         self.current_time = current_time
         self.total_distance = 0.0  # Initialize total distance to zero
+        self.required_packages = required_packages
 
-    def __str__(self):
-        return "%s, %s, %s, %s, %s, %s, %s, %s, %s ,%s" % (self.id, self.max_load, self.velocity, self.cargo,
-                                                           self.shipments, self.distance_travelled,
-                                                           self.departure_time, self.current_address,
-                                                           self.current_time, self.distance_travelled)
+    def showdata(self):
+        print(f"\n=========TRUCK {self.id}===========")
+        print(f"Departure time: {self.departure_time}")
+        print(f"Package count: {self.cargo}")
+        print(f"Packages: {self.shipments}")
+        print(f"Distance travelled: {self.distance_travelled:.2f}")
+        for f in self.required_packages: print(f.pid)
